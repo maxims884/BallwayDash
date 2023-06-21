e88d022cc8083cf54d9be11d043650fa7d3b8a77
maxims884 <maxims884@gmail.com>
2023-06-21 21:52:52 +0600

    add info button to main menu

---
+++ Assets/Scripts/MainMenuClickInfoButton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuClickInfoButton : MonoBehaviour,IPointerDownHandler,IPointerUpHandler
+{
+	[SerializeField] private Image img;
+	[SerializeField] private Sprite _default, pressed;
+	[SerializeField] private AudioClip pressClip, unPressClip;
+	[SerializeField] private AudioSource source;
+    
+public void OnPointerUp(PointerEventData eventData){
+	img.sprite = _default;
+	source.PlayOneShot(unPressClip);
+}
+public void OnPointerDown(PointerEventData eventData){
+	img.sprite = pressed;
+	source.PlayOneShot(pressClip);
+
+}
+
+public void wasClicked(){
+	Debug.Log("Clicked ");
+//SceneManager.LoadScene(SceneManager.GetActiveScene().buildI//ndex + 1);
+
+}
+}
